7d85ffec2b899f5bc0b91bb0406e099acd7f1030
Viko <vikotodorov02@gmail.com>
2020-06-09 17:37:33 +0300

    add indexes and IDs

--- App/database/create.py
+++ App/database/create.py
@@ -15,14 +15,20 @@ def createDB():
                         Name VARCHAR(256) );''')
     
     conn.cursor().execute('''CREATE TABLE IF NOT EXISTS Books
-                        (ISBN VARCHAR(17),
+                        (ID INT PRIMARY KEY,
+                        ISBN VARCHAR(17),
                         Title VARCHAR(256),
-                        PublishedDate DATE,
-                        PRIMARY KEY (ISBN));''')
+                        PublishedDate DATE);''')
     
     conn.cursor().execute('''CREATE TABLE IF NOT EXISTS AuthorsBooks
-                        (ISBN_b VARCHAR(17),
+                        (ID_b INT,
                         ID_auth INT,
-                        PRIMARY KEY (ISBN_b, ID_auth),
-                        FOREIGN KEY (ISBN_b) REFERENCES Books(ISBN),
+                        PRIMARY KEY (ID_b, ID_auth),
+                        FOREIGN KEY (ID_b) REFERENCES Books(ID),
                         FOREIGN KEY (ID_auth) REFERENCES Authors(ID) );''')
+
+    conn.cursor().execute('''CREATE INDEX Authors_name
+                          ON Authors (Name)''')
+
+    conn.cursor().execute('''CREATE INDEX Books_name
+                          ON Books (Title)''')
