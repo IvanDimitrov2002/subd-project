9a91482ca4fe0350f36fe5ae47e30f6e5f2da65e
Kaloyan Angelov <kaloyan.angelov.tues@gmail.com>
2020-06-12 01:04:41 +0300

    [Update] some book.py updates

--- App/main.py
+++ App/main.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask import render_template
 # from flask import url_for
-import database.create as database
 
+import database.create as database
 from models.book import Book
 
 app = Flask(__name__)
@@ -21,4 +21,13 @@ def view_all_books(book_name, methods):
 
 if __name__ == '__main__':
     database.createDB()
+
+    books = [Book("Lady Midnight", "Adventure", "2020-03-13", "12319123"),
+             Book("Lord of Shadows", "Adventure", "2018-03-13", "12314222"),
+             Book("How to program", "Info", "2016-03-09", "54569123"),
+             Book("Giving you them", "Info", "2021-05-13", "12319123")]
+
+    for book in books:
+        book.add_book()
+
     app.run(debug=True)
